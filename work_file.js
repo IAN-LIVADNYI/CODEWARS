8250fc702e38bb063802d4c18c581454846ca2de
IAN-LIVADNYI <livadny.yan@gmail.com>
2022-04-13 23:54:01 -0400

    new solved kata

--- work_file.js
+++ work_file.js
@@ -1,5 +1,14 @@
-function isLetter(c) {
-    return c.toLowerCase() != c.toUpperCase();
+function eachCons(array, n) {
+    let arrNew = [];
+    for(let k = 0; k < array.length-n+1; k++){
+        let litleArr = []
+        for(let i = k; i < n+k; i++){
+            litleArr.push(array[i])
+        }
+        arrNew.push(litleArr)
+    }
+    return arrNew;
 }
 
-console.log(isLetter("1"))
+console.log(eachCons([1,2,3,4], 2));
+//[[1,2,3],[2,3,4]]
