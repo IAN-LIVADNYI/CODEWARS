12599f8907ebe0aa65cd57b52a35e64774159d33
IAN-LIVADNYI <livadny.yan@gmail.com>
2022-04-25 23:26:17 -0400

    new kata solved

--- work_file.js
+++ work_file.js
@@ -1,17 +1,28 @@
-let arr = [1,2,6,8,6,8,9,5,4,6,5,3,1,4,4,2,5,4,5,8,9,5,6,3,1,4,7,5,6,3,3];
-function findFirstDuplicate(arr){
+// let arr = [1,2,6,8,6,8,9,5,4,6,5,3,1,4,4,2,5,4,5,8,9,5,6,3,1,4,7,5,6,3,3];
+// function findFirstDuplicate(arr){
+//
+// };
+// console.log(findFirstDuplicate(arr))
+//
+//
+//
+//
+// let str = ['fshsklooejebcijbckclsmsbssllltuwopqqnabzvxxp']
+// const ascendantArr = str =>{
+//
+// };
 
-};
-console.log(findFirstDuplicate(arr))
 
-
-
-
-let str = ['fshsklooejebcijbckclsmsbssllltuwopqqnabzvxxp']
-const ascendantArr = str =>{
-
-};
-
-
-
+function mergeArrays(arr1, arr2) {
+   let newArr = [...arr1].concat(...arr2).sort((a,b) => a-b);
+   let resArr = [];
+   // for(let i = 0; i < newArr.length; i++){
+   //    if(newArr.lastIndexOf(i) === newArr.indexOf(i)){
+   //       resArr.push(newArr[i])
+   //    }
+   // }
+   return newArr;
+   // return resArr;
+}
+console.log( mergeArrays([8, 2, 8, 4, 5], [6, 1, 8, 9, 10]))
 
