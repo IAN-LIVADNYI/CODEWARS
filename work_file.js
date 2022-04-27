a8cbd21384294f4e0de4347baa83fdb1ca3cf67a
IAN-LIVADNYI <livadny.yan@gmail.com>
2022-04-26 23:40:44 -0400

    new solved problem added

--- work_file.js
+++ work_file.js
@@ -1,21 +1,13 @@
-// let arr = [1,2,6,8,6,8,9,5,4,6,5,3,1,4,4,2,5,4,5,8,9,5,6,3,1,4,7,5,6,3,3];
-// function findFirstDuplicate(arr){
-//
-// };
-// console.log(findFirstDuplicate(arr))
-//
-//
-
-
-const ascendantArr = str => {
-let arr = (str.split('').sort());
-let res = [];
-for (let i = 0; i < arr.length; i++){
-    if(arr[i] !== arr[i + 1]){
-        res.push(arr[i])
+let arr = [1,2,6,8,6,8,9,5,4,6,5,3,9,4,4,5,4,5,8,9,5,6,3,5,4,7,5,6,3,3,10];
+function findFirstDuplicate(arr){
+    for(let i = 0; i < arr.length; i++){
+        for(let k = i+1; k < arr.length; k++){
+            if(arr[i] === arr[k]) return arr[k];
+        }
     }
-}
-return (res)
+return "no doubles";
 };
+console.log(findFirstDuplicate(arr))
 
-console.log(ascendantArr('fshsklooejebcijbckclsmsbssllltuwopdaaaaqqnabzvxxpa'))
+
+
