dfc6acf0d398cb7a9165e612f30f9b211a8b2bd5
IAN-LIVADNYI <livadny.yan@gmail.com>
2022-05-09 01:20:25 -0400

    work commit

--- work_file.js
+++ work_file.js
@@ -1,15 +1,25 @@
 //https://www.codewars.com/kata/602afedfd4a64d0008eb4e6e/train/javascript
 function getDay(day,isLeap) {
-    let arrMonth = [['January', 31, 31], ['February', 28, 29], ['March', 30, 30], ['April', 31, 31], ['May', 30, 30], ['June', 31, 31], 'July', 'August', 'September', 'October', 'November', 'December'];
+    let arrMonth = [['January', 31, 31], ['February', 28, 29], ['March', 31, 31], ['April', 30, 30], ['May', 31, 31], ['June', 30, 30], ['July',31,31], ['August',31,31], ['September',30,30], ['October',31,31], ['November',30,30], ['December',31,31], ['December',0,0]];
     if (day > 31 && isLeap === false) {
         let printRes;
-        let res;
-   for(let i = 0;res < 0;i++){
-       res = day - arrMonth[i][1];
-       printRes = `${arrMonth[i][0]}, ${res}`
-   }
+        for (let i = 0; day > 0 || i > arrMonth.length-1; i++) {
+            day = day - arrMonth[i][1];
+            if ((day - arrMonth[i + 1][1]) < 0) {
+                printRes = `${arrMonth[i][0]}, ${day+arrMonth[i][1]}`;
+            }
+            else if ((day - arrMonth[i + 1][1]) === 0) {
+                printRes = `${arrMonth[i][0]}, ${arrMonth[i][1]}`
+            }
+            else {
+                printRes = `${arrMonth[i + 2][0]}, ${day - arrMonth[i + 1][1]}`
+            }
+        }
         return printRes;
     }
     return `January, ${day}`
 }
-console.log(getDay(32, false))
+console.log(getDay(182, false))
+
+//(arrMonth[i+1][1]
+// for (let i = 0; day > 0; i++) {
