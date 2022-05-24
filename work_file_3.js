a165db5f5935b2f725bf28933d7c467aef00f0ab
IAN-LIVADNYI <livadny.yan@gmail.com>
2022-05-23 23:18:36 -0400

    work kata

--- work_file_3.js
+++ work_file_3.js
@@ -1,6 +1,6 @@
 //1. Create 5 variables, initialize them with integers and
 // find the average of variables. Print the result to console
-
-
-//2. Create a variable, initialize it with a positive integer.
-// Make it negative and print the result to console
+let variables = [{a : 1}, {b : 2}, {c : 3}, {d : 4}, {e : 5}];
+let sum = 0;
+variables.map(el => variables[1])
+console.log(sum);
