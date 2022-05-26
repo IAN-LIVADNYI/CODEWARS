2f0ff85716dccb550ecd66eb018e29ea6194c5f4
IAN-LIVADNYI <livadny.yan@gmail.com>
2022-05-25 23:45:51 -0400

    working commit

--- work_file_3.js
+++ work_file_3.js
@@ -1,5 +1,4 @@
-//1. Create 5 variables, initialize them with integers and
-// find the average of variables. Print the result to console
+//1. Create 5 variables, initialize them with integers and find the average of variables. Print the result to console
 let variables = [{a : 1}, {b : 2}, {c : 3}, {d : 4}, {e : 5}];
 let sum = 0;
 variables.map(el => variables[1])
